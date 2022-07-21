48873e26f6bbabd80cc4248b6c530543569f1bdd
sammy-vpd <sammy.vpd@gmail.com>
2022-07-21 09:58:49 +0800

    Sidebar menu v0.1

--- sidebar.php
+++ sidebar.php
@@ -10,9 +10,9 @@
           </button>
           <div class="collapse" id="employee-list" style="">
             <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-              <li><a href="./index1.php?page=display">List Of Employees</a></li>
-              <li><a href="">Employee1</a></li>
-              <li><a href="">Employee2</a></li>
+              <li><a href="./index1.php?page=display" class="link-dark rounded">List Of Employees</a></li>
+              <li><a href="" class="link-dark rounded">Employee1</a></li>
+              <li><a href="" class="link-dark rounded">Employee2</a></li>
             </ul>
           </div>
         </li>
@@ -22,49 +22,64 @@
           </button>    
           <div class="collapse" id="item-list" style="">
             <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-              <li><a href="">Item1</a></li>
-              <li><a href="">Item2</a></li>
-              <li><a href="">Item3</a></li>
+              <li><a href="" class="link-dark rounded">Item1</a></li>
+              <li><a href="" class="link-dark rounded">Item2</a></li>
+              <li><a href="" class="link-dark rounded">Item3</a></li>
             </ul>
           </div>
         </li>
-
-        <li><a href="">Inventory</a>
-          <ul>
-            <li><a href="">Inventory1</a></li>
-            <li><a href="">Inventory2</a></li>
-            <li><a href="">Inventory3</a></li>
-          </ul>
-        </li>
-        <li><a href="">Customer</a>
-          <ul>
-            <li><a href="">Customer1</a></li>
-            <li><a href="">Customer2</a></li>
-            <li><a href="">Customer3</a></li>
-          </ul>
-          <li><a href="">Settings</a>
-            </li>
-            <li><a href="">Helps</a>
-              <ul>
-              <li><a href="">Hepl1</a></li>
-              <li><a href="">Help2</a></li>
-              <li><a href="">Hep3</a></li>
+        <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#inventory-list" aria-expanded="false">
+            Inventory
+          </button>
+          <div class="collapse" id="inventory-list" style="">
+            <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+              <li><a href="" class="link-dark rounded">Inventory1</a></li>
+              <li><a href="" class="link-dark rounded">Inventory2</a></li>
+              <li><a href="" class="link-dark rounded">Inventory3</a></li>
             </ul>
+          </div>
         </li>
-        <li><a href="db/logout.php">Logout</a></li>
+        <li class="mb-1">
+        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#customer-list" aria-expanded="false">
+            Customer
+          </button>
+          <div class="collapse" id="customer-list" style="">
+            <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+              <li><a href="" class="link-dark rounded">Customer1</a></li>
+              <li><a href="" class="link-dark rounded">Customer2</a></li>
+              <li><a href="" class="link-dark rounded">Customer3</a></li>
+            </ul>
+          </div>
+        </li>
+        <li class="mb-1">
+          <a href="settings.php" class="btn btn-toggle align-items-center rounded collapsed">
+            Settings
+          </a>
+        </li>
+        <li class="mb-1">
+          <a href="help.php" class="btn btn-toggle align-items-center rounded collapsed">
+            Help
+          </a>
+        </li>
+        <li class="mb-1">
+          <a href="db/logout.php" class="btn btn-toggle align-items-center rounded collapsed">
+            Logout
+          </a>
+        </li>
       </ul>
     </div>
-    <div class="sidebox">
-      <ul class="share">
-        <li><a href="#"><img src="assets/imgs/icon-rss.png" alt=""></a></li>
-        <li><a href="#"><img src="assets/imgs/icon-facebook.png" alt=""></a></li>
-        <li><a href="#"><img src="assets/imgs/icon-twitter.png" alt=""></a></li>
-        <li><a href="#"><img src="assets/imgs/icon-dribbble.png" alt=""></a></li>
-        <li><a href="#"><img src="assets/imgs/icon-linkedin.png" alt=""></a></li>
-      </ul>
-    </div>
+  <div class="sidebox">
+    <ul class="share">
+      <li><a href="#"><img src="assets/imgs/icon-rss.png" alt=""></a></li>
+      <li><a href="#"><img src="assets/imgs/icon-facebook.png" alt=""></a></li>
+      <li><a href="#"><img src="assets/imgs/icon-twitter.png" alt=""></a></li>
+      <li><a href="#"><img src="assets/imgs/icon-dribbble.png" alt=""></a></li>
+      <li><a href="#"><img src="assets/imgs/icon-linkedin.png" alt=""></a></li>
+    </ul>
+  </div>
     <div class="sidebox" style="color:#FFF;">
       <p style="margin-bottom:5px;">Copyright &copy; VPD Business Solutions</p>
       <p>Design By: <a href="http://elemisfreebies.com">VPD</a></p>
     </div>
-  </div>
+</div>
